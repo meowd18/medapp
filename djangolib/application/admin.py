0d40c0e06229a887de1e72ee7fe117dc8939a390
Maud <m-aud.b@live.fr>
2023-10-19 09:39:52 +0200

    affichage des réponses des forms dans admin

--- djangolib/application/admin.py
+++ djangolib/application/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from application.models import ColSante, ColStress
 
-# Register your models here.
+
+class ColonnesStress(admin.ModelAdmin):
+    #list_display = ("id", "username", "role", "email","is_superuser",)  #[field.name for field in Utilisateur._meta.get_fields()]
+    list_display = [field.name for field in ColStress._meta.get_fields()]
+
+class ColonnesSante(admin.ModelAdmin):
+    #list_display = ("id", "username", "role", "email","is_superuser",)  #[field.name for field in Utilisateur._meta.get_fields()]
+    list_display = [field.name for field in ColSante._meta.get_fields()]
+
+admin.site.register(ColStress, ColonnesStress)
+admin.site.register(ColSante, ColonnesSante)
