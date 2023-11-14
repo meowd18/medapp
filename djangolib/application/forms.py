23b70b6deb92c759aad28f64e65f8cdbb4185576
Maud <m-aud.b@live.fr>
2023-11-14 15:45:12 +0100

    virgule pour float, pas pour integer

--- djangolib/application/forms.py
+++ djangolib/application/forms.py
@@ -25,6 +25,18 @@ class ColSanteForm(forms.ModelForm):
             if isinstance(field, (forms.IntegerField, models.IntegerField)) and field.name != 'id'
         }
 
+        text_widgets = [
+            'tour_de_taille_en_cm', 'poids', 'fréquence_cardiaque_par_minute',
+            'tension_artérielle_systolique_prise_du_matin', 'tension_artérielle_systolique_prise_du_soir',
+            'tension_artérielle_diastolique_prise_du_matin', 'tension_artérielle_diastolique_prise_du_soir',
+            'nombre_de_médicaments_pris_dans_la_journée', 'nombre_de_repas_pris_durant_la_journée',
+            'durée_de_lactivité_physique_en_minutes', 'durée_totale_des_palpitations_en_minutes',
+            'durée_totale_des_douleurs_thoraciques_en_minutes', 'durée_totale_des_malaises_en_minutes',
+        ]
+
+
+        for field_name in text_widgets:
+            widgets[field_name] = forms.TextInput(attrs={'type': 'text', 'data-integerfield': 'true'})
     def __init__(self, *args, **kwargs):
         super(ColSanteForm, self).__init__(*args, **kwargs)
         initial = kwargs.get('initial', {})
