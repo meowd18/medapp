7445156cf9db6d073ff36b14477ee5f0d92ff065
Maud <m-aud.b@live.fr>
2024-04-04 11:17:56 +0200

    check connexion & re-open

--- djangolib/application/utils.py
+++ djangolib/application/utils.py
@@ -1,6 +1,6 @@
-import random, logging, mlflow, os, sqlite3, requests
-from django.db import connections
-
+import random, logging, mlflow, os, sqlite3, requests, traceback
+from django.db import connections, OperationalError, reset_queries, close_old_connections
+from django.conf import settings
 logging.basicConfig(level=logging.DEBUG)
 
 def init_mlflow():
@@ -23,21 +23,50 @@ def send_alert_discord(subject, body):
     except Exception as e:
         logging.info(f'Error sending Discord alert: {e}')
 
+def check_default_database_existence():
+    # Récupérer le chemin de la base de données à partir des paramètres de configuration de Django
+    db_path = settings.DATABASES['default']['NAME']
+
+    # Vérifier si le fichier de base de données existe
+    if os.path.exists(db_path):
+        print(f"La base de données par défaut existe à l'emplacement: {db_path}")
+        return True
+    else:
+        print(f"La base de données par défaut n'existe pas à l'emplacement: {db_path}")
+        return False
+
+def check_database_connection(cursor):
+    try:
+        # Exécutez une requête simple ici, par exemple :
+        cursor.execute("SELECT 1")
+        # Si la requête s'exécute sans erreur, cela signifie que la connexion fonctionne correctement
+        print("La connexion à la base de données fonctionne correctement.")
+        return True
+    except Exception as e:
+        # En cas d'erreur de connexion
+        print("Erreur lors de la connexion à la base de données:", e)
+        return False
 
 def handle_bug(form, form_name, prenom):
     # Générer un nombre aléatoire pour déterminer s'il y aura un bug
     x = random.randint(1, 100)
     bdd = 'default'
     with connections[bdd].cursor() as cursor:
-        if (x % 2) == 0:
+        if x == 0:
             logging.debug(f'{x}: no bug should arise')
         else:
             logging.debug(f'{x}: bug inserted')
             cursor.connection.close()
-            bdd = 'bddlib'
-            # Modifier temporairement la configuration de la base de données par défaut pour simuler un bug
-    with connections[bdd].cursor() as cursor:
-        try:
+
+    try:
+        if not check_default_database_existence(): #si la bdd n'est pas trouvée
+            pass #prévenir utilisateur, bloquer formulaires, envoyer alerte
+        if not check_database_connection(cursor): #si la connexion est fermée
+            reset_queries()
+            close_old_connections()
+            connections[bdd].ensure_connection()
+            print("La connexion à la base de données a été rétablie avec succès.")
+        with connections[bdd].cursor() as cursor:
             form.save()
             logging.debug('insertion successful')
             with mlflow.start_run():
@@ -46,106 +75,103 @@ def handle_bug(form, form_name, prenom):
                 mlflow.log_param("user", prenom)
                 message = 'Votre formulaire a bien été enregistré'
                 return message
-        except Exception as e:
-            logging.debug('insertion failed')
-            with mlflow.start_run():
-                mlflow.log_metric("is_insertion_successful", 0)
-                mlflow.log_param("error_message", str(e))
-                mlflow.log_param("form", form_name)
-                mlflow.log_param("user", prenom)
-                message = "Un problème est survenu lors de l'enregistrement de votre formulaire, veuillez réessayer. Si l'erreur persiste, merci de contacter votre médecin traitant."
-                return message
+    except Exception as e:
+        print(str(e))
+        traceback.print_exc()
+        logging.debug('insertion failed')
+        with mlflow.start_run():
+            mlflow.log_metric("is_insertion_successful", 0)
+            mlflow.log_param("error_message", str(e))
+            mlflow.log_param("form", form_name)
+            mlflow.log_param("user", prenom)
+            message = "Un problème est survenu lors de l'enregistrement de votre formulaire, veuillez réessayer ultérieurement. Si l'erreur persiste, merci de contacter votre médecin traitant."
+            return message
 
-        finally:
-            #dans tous les cas, on vérifie le seuil
-            #le cas échéant on envoie une alerte discord
-            #connexion à la base de données utilisée par MLflow
-            conn = sqlite3.connect('/home/greta/alternance/medapp/mlflow.db')
-            cursor = conn.cursor()
-            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    finally:
+        #dans tous les cas, on vérifie le seuil
+        #le cas échéant on envoie une alerte discord
+        #connexion à la base de données utilisée par MLflow
+        conn = sqlite3.connect('/home/greta/alternance/medapp/mlflow.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 
-            # Récupérer les résultats de la requête
-            tables = cursor.fetchall()
+        # Récupérer les résultats de la requête
+        tables = cursor.fetchall()
 
-            # Afficher les noms des tables présentes dans la base de données
-            print("Tables présentes dans la base de données :")
-            for table in tables:
-                print(table[0])
+        #nombre de valeurs à prendre en compte, ou x dernières valeurs
+        metrics = 3
+        #seuil d'alerte
+        threshold = 2
+        #requête SQL pour récupérer toutes les valeurs de métriques
+        cursor.execute(f'SELECT value FROM metrics ORDER BY timestamp DESC')
+        values = cursor.fetchall()
+        #requête SQL pour récupérer les x dernières valeurs
+        cursor.execute(f'SELECT value FROM metrics ORDER BY timestamp DESC LIMIT {metrics}')
+        x_last_values = cursor.fetchall()
+        #requête SQL pour récupérer les messages d'erreur des x derniers échecs
+        cursor.execute(f'SELECT params.value FROM params'
+                       f' JOIN metrics ON metrics.run_uuid = params.run_uuid'
+                       f' WHERE params.key = "error_message" AND metrics.value = 0'
+                       f' AND metrics.timestamp IN (SELECT timestamp FROM metrics ORDER BY timestamp DESC LIMIT {metrics})'
+                       f' ORDER BY metrics.timestamp DESC')
+        x_last_errors = cursor.fetchall()
+        #requête SQL pour récupérer les formulaires correspondant aux messages d'erreur
+        cursor.execute(f'SELECT params.value FROM params'
+                       f' JOIN metrics ON metrics.run_uuid = params.run_uuid'
+                       f' WHERE params.key = "form" AND metrics.value = 0 AND metrics.timestamp'
+                       f' IN (SELECT timestamp FROM metrics ORDER BY timestamp DESC LIMIT {metrics})'
+                       f' ORDER BY metrics.timestamp DESC')
+        failed_forms = cursor.fetchall()
+        logging.debug(f"x dernières valeurs: {x_last_values}")
+        logging.debug(f"x dernières erreurs: {x_last_errors}")
+        logging.debug(f"formulaires: {failed_forms}")
+        #nombre d'échecs parmi les x dernières valeurs
+        fail = sum(val[0] == 0 or val[0] == 0.0 for val in x_last_values)
+        logging.debug(f"nombre d'échecs: {fail}")
 
-            #nombre de valeurs à prendre en compte, ou x dernières valeurs
-            metrics = 3
-            #seuil d'alerte
-            threshold = 2
-            #requête SQL pour récupérer toutes les valeurs de métriques
-            cursor.execute(f'SELECT value FROM metrics ORDER BY timestamp DESC')
-            values = cursor.fetchall()
-            #requête SQL pour récupérer les x dernières valeurs
-            cursor.execute(f'SELECT value FROM metrics ORDER BY timestamp DESC LIMIT {metrics}')
-            x_last_values = cursor.fetchall()
-            #requête SQL pour récupérer les messages d'erreur des x derniers échecs
-            cursor.execute(f'SELECT params.value FROM params'
-                           f' JOIN metrics ON metrics.run_uuid = params.run_uuid'
-                           f' WHERE params.key = "error_message" AND metrics.value = 0'
-                           f' AND metrics.timestamp IN (SELECT timestamp FROM metrics ORDER BY timestamp DESC LIMIT {metrics})'
-                           f' ORDER BY metrics.timestamp DESC')
-            x_last_errors = cursor.fetchall()
-            #requête SQL pour récupérer les formulaires correspondant aux messages d'erreur
-            cursor.execute(f'SELECT params.value FROM params'
-                           f' JOIN metrics ON metrics.run_uuid = params.run_uuid'
-                           f' WHERE params.key = "form" AND metrics.value = 0 AND metrics.timestamp'
-                           f' IN (SELECT timestamp FROM metrics ORDER BY timestamp DESC LIMIT {metrics})'
-                           f' ORDER BY metrics.timestamp DESC')
-            failed_forms = cursor.fetchall()
-            logging.debug(f"x dernières valeurs: {x_last_values}")
-            logging.debug(f"x dernières erreurs: {x_last_errors}")
-            logging.debug(f"formulaires: {failed_forms}")
-            #nombre d'échecs parmi les x dernières valeurs
-            fail = sum(val[0] == 0 or val[0] == 0.0 for val in x_last_values)
-            logging.debug(f"nombre d'échecs: {fail}")
+        # Fermeture de la connexion à la base de données
+        conn.close()
 
-            # Fermeture de la connexion à la base de données
-            conn.close()
+        #on vérifie qu'il y a assez de valeurs, que le nombre d'échecs est atteint et que la dernière tentative est un échec
+        if len(values) >= metrics and fail >= threshold and x_last_values[0][0] == 0:
+            logging.debug('seuil atteint')
 
-            #on vérifie qu'il y a assez de valeurs, et que le nombre d'échecs est atteint
-            if len(values) >= metrics and fail >= threshold and x_last_values[0][0] == 0:
-                logging.debug('seuil atteint')
+            #liste pour stocker les erreurs avec les formulaires associés
+            errors_and_forms = []
+            unique_errors = set()
+            unique_forms = set()
 
-                #liste pour stocker les erreurs avec les formulaires associés
-                errors_and_forms = []
-                unique_errors = set()
-                unique_forms = set()
+            #parcourir les erreurs récentes et les formulaires associés
+            for val, form in zip(x_last_errors, failed_forms):
+                error_message = val[0]
+                form_name = form[0]
 
-                #parcourir les erreurs récentes et les formulaires associés
-                for val, form in zip(x_last_errors, failed_forms):
-                    error_message = val[0]
-                    form_name = form[0]
+                #vérifier si l'erreur est unique
+                if error_message not in unique_errors:
+                    errors_and_forms.append({"error": error_message, "forms": {form_name}})
+                    unique_errors.add(error_message)
+                else:
+                    #ajouter le formulaire associé à une erreur existante
+                    for entry in errors_and_forms:
+                        if entry["error"] == error_message:
+                            entry["forms"].add(form_name)
 
-                    #vérifier si l'erreur est unique
-                    if error_message not in unique_errors:
-                        errors_and_forms.append({"error": error_message, "forms": {form_name}})
-                        unique_errors.add(error_message)
-                    else:
-                        #ajouter le formulaire associé à une erreur existante
-                        for entry in errors_and_forms:
-                            if entry["error"] == error_message:
-                                entry["forms"].add(form_name)
+                #ajouter le formulaire associé
+                unique_forms.add(form_name)
 
-                    #ajouter le formulaire associé
-                    unique_forms.add(form_name)
+            #formater les problèmes en une chaîne lisible
+            formatted_issues = ""
+            counter = 1
 
-                #formater les problèmes en une chaîne lisible
-                formatted_issues = ""
-                counter = 1
+            for entry in errors_and_forms:
+                error_message = entry["error"]
+                forms_associated = ", ".join(entry["forms"])
 
-                for entry in errors_and_forms:
-                    error_message = entry["error"]
-                    forms_associated = ", ".join(entry["forms"])
+                formatted_issues += f"\n ⚠️ {counter}. Erreur: {error_message}\n Formulaire(s) associé(s): {forms_associated}\n{'*' * 20}\n"
+                counter += 1
 
-                    formatted_issues += f"\n ⚠️ {counter}. Erreur: {error_message}\n Formulaire(s) associé(s): {forms_associated}\n{'*' * 20}\n"
-                    counter += 1
-
-                issue = formatted_issues.rstrip('\n') if formatted_issues else "Aucune erreur n'a été trouvée."
-                msg = "Echec de l'enregistrement de vos données, veuillez réessayer ultérieurement. Si le problème persiste, contactez votre médecin traitant."
-                #générer le message d'alerte pour Discord
-                send_alert_discord("Alerte MLflow", f"Le seuil d'échecs ({threshold} envois sur {metrics}) a été atteint."
-                                                    f"\n Le ou les problèmes suivants ont causé l'alerte: {issue}")
+            issue = formatted_issues.rstrip('\n') if formatted_issues else "Aucune erreur n'a été trouvée."
+            msg = "Echec de l'enregistrement de vos données, veuillez réessayer ultérieurement. Si le problème persiste, contactez votre médecin traitant."
+            #générer le message d'alerte pour Discord
+            send_alert_discord("Alerte MLflow", f"Le seuil d'échecs ({threshold} envois sur {metrics}) a été atteint."
+                                                f"\n Le ou les problèmes suivants ont causé l'alerte: {issue}")
